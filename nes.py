91b52d77e5b31694368ff763dbd3800469100ad3
K.Tsukimoto <t.29.wuq@gmail.com>
2018-10-25 16:53:15 +0900

    romの分割

--- nes.py
+++ nes.py
@@ -6,7 +6,14 @@ Executable File
 """
 
 if __name__ == '__main__':
-    ron = b''
     with open(sys.argv[1], 'rb') as f:
-        print(f.read())
+        rom = bytearray(f.read())
+    
+    count = 0x10
+    program_rom = rom[0x10:0x10 + rom[4] * 0x4000]
+    character_rom = rom[0x10 + rom[4] * 0x4000:0x10 + rom[4] * 0x4000 + rom[5] * 0x2000]
+    print(len(program_rom))
+    print(len(character_rom))
+    print(rom[4])
+
     
