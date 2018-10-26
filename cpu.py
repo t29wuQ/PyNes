46f42539721b3f7da9d56421f7138098f0eed872
K.Tsukimoto <t.29.wuq@gmail.com>
2018-10-26 18:08:02 +0900

    add : lda ldx ldy

--- cpu.py
+++ cpu.py
@@ -6,12 +6,40 @@ CPU6502
 
 class Cpu:
     def __init__(self, program_rom):
-        self.memory = bytearray(b'0' * 0x10000)
+        self.memory = bytearray(b'0' * 0x8000)
         self.registerA = 0 
         self.registerX = 0
         self.registerY = 0
-        self.registerS = 0
-        self.program_counter = 0x8000
+        self.registerS = 0xff
+        self.memory += program_rom
+        self.n_flag = 0
+        self.v_flag = 0
+        self.b_flag = 0
+        self.i_flag = 0
+        self.z_flag = 0
+        self.c_flag = 0
+
+    def test(self, value):
+        self.memory[2] = value
+        print(self.memory[2])
+
+
+    def fetch_memory(self, address):
+        return self.memory[address]
+
+    def write_memory(self, address, value):
+        self.memory[address] = value
+
+    def check_flag_n_z(self, value):
+        if self.z_flag == 0:
+            self.z_flag = 1
+        else:
+            self.z_flag = 0
+        self.n_flag
+
+    """
+    アドレッシングここから
+    """
 
     def immediate(self):
         address = self.program_counter + 1
@@ -47,3 +75,42 @@ class Cpu:
         address = (self.memory[self.program_counter + 2] * 0x100 + self.memory[self.program_counter + 1]) & 0xffff
         self.program_counter += 3
         return (address + self.registerY) & 0xffff
+
+    """
+    アドレッシングここまで
+    """
+
+
+
+    """
+    命令セットここから
+    """
+
+    def lda(self, address):
+        self.registerA = self.FetchMemory(adderess) & 0xff
+        self.check_flag_n_z(self.registerA)
+
+    def ldx(self, address):
+        self.registerX= self.FetchMemory(adderess) & 0xff
+        self.check_flag_n_z(self.registerX)
+
+    def ldy(self, address):
+        self.registerY = self.FetchMemory(adderess) & 0xff
+        self.check_flag_n_z(self.registerY)
+
+    
+    def adc(self, address):
+        sum = self.registerA + self.fetch_memory(address) + self.c_flag
+        self.registerA = sum & 0xff
+        self.check_flag_n_z(self.registerA)
+        self.v_flag = 
+        if sum > 0xff:
+            self.c_flag = 1
+        else:
+            self.c_flag = 0
+        
+
+
+    """
+    命令セットここまで
+    """
